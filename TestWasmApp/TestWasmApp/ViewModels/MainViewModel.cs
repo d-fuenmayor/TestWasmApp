fc4a190cdb2591defd418d002e3f791d102bea9c
Daniel Fuenmayor <d-fuenmayor@github.com>
2024-04-19 18:13:53 -0400

    Adding to the default greeting to see if this will build on Azure Static Web Apps with CI/CD

--- TestWasmApp/TestWasmApp/ViewModels/MainViewModel.cs
+++ TestWasmApp/TestWasmApp/ViewModels/MainViewModel.cs
@@ -3,6 +3,6 @@
 public class MainViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
+    public string Greeting => "Welcome to Avalonia! This is a placeholder app made by Daniel Alfonso Fuenmayor that proves he can do CI/CD for an Avalonia WASM Browser App!";
 #pragma warning restore CA1822 // Mark members as static
 }
